429e52f5e0f869368c20d2bb4ee299e0394e1ed6
htmltest <struev.vs@gmail.com>
2015-10-27 14:49:41 +0300

    add 2 question

--- js/tools.js
+++ js/tools.js
@@ -17,6 +17,14 @@
             }
         });
 
+        $('.question-radio-item input:checked').parent().addClass('checked');
+        $('.question-radio-item').click(function() {
+            var curName = $(this).find('input').attr('name');
+            $('.question-radio-item input[name="' + curName + '"]').parent().removeClass('checked');
+            $(this).addClass('checked');
+            $(this).find('input').prop('checked', true).trigger('change');
+        });
+
     });
 
 })(jQuery);
